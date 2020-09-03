a32f7e40049b8f2f1ac26ca647e628aa452e124b
Anna Vondrukhova <anya1312@yandex.ru>
2020-09-03 10:19:22 +0300

    All login storyboard functionality transferred to LoginViewController

--- SplitTheCheck/LoginViewControllers/LoginViewController.swift
+++ SplitTheCheck/LoginViewControllers/LoginViewController.swift
@@ -9,15 +9,18 @@
 import UIKit
 import SwiftyJSON
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet weak var scrollView: UIScrollView!
+class LoginViewController: UIViewController, UITextFieldDelegate, UIViewControllerTransitioningDelegate {
     @IBOutlet weak var loginText: UITextField!
     @IBOutlet weak var pwdText: UITextField!
     @IBOutlet weak var forgetBtn: UIButton!
-    @IBOutlet weak var logInBtn: CustomButton!
     @IBOutlet weak var signUpBtn: CustomButton!
+    @IBOutlet weak var checkbox: Checkbox!
+    @IBOutlet weak var iAcceptLabel: UILabel!
+    @IBOutlet weak var logInBtn: CustomButton!
     @IBOutlet weak var waitingView: UIView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    var agreementBtn: UIButton = UIButton()
+    let url = "https://lkfl2.nalog.ru/lkfl/login"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +38,10 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tap.cancelsTouchesInView = false
         self.view.addGestureRecognizer(tap)
+        
+        logInBtn.isEnabled = false
+        logInBtn.backgroundColor = UIColor(red:0.75, green:0.75, blue:0.75, alpha:1.0)
+        checkbox.delegate = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -44,6 +51,30 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         NSLog ("LoginVC will appear")
     }
     
+    override func viewWillLayoutSubviews() {
+        setUpAgreementBtn()
+    }
+    
+    func setUpAgreementBtn() {
+        agreementBtn.setTitleColor(UIColor(red:0.37, green:0.75, blue:0.62, alpha:1.0), for: .normal)
+        agreementBtn.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        agreementBtn.setTitle("пользовательского соглашения", for: .normal)
+        
+        if self.view.frame.width < 370 {
+            agreementBtn.frame = CGRect(x: 42, y: iAcceptLabel.frame.minY + 15, width: 193, height: 15)
+            print ("\(agreementBtn.frame.minX), \(agreementBtn.frame.minY)")
+        } else {
+            agreementBtn.frame.size = CGSize(width: 193, height: 15)
+            agreementBtn.center.y = iAcceptLabel.center.y
+            agreementBtn.frame.origin.x = iAcceptLabel.frame.maxX
+            print ("\(iAcceptLabel.frame.maxX), \(iAcceptLabel.frame.minY)")
+            print ("\(agreementBtn.frame.minX), \(agreementBtn.frame.minY)")
+        }
+        self.view.addSubview(agreementBtn)
+        self.view.bringSubview(toFront: agreementBtn)
+        agreementBtn.addTarget(self, action: #selector(agreementBtnTap(_:)), for: .touchUpInside)
+    }
+        
     func textFieldDidBeginEditing(_ textField: UITextField) {
         print ("did begin editing")
         if ((textField == loginText)&&(textField.text == "")) {
@@ -56,6 +87,28 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     {
         view.endEditing(true)
     }
+    
+    //вызываем экран с пользовательским соглашением
+    @objc func agreementBtnTap(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let userAgreementVC = storyboard.instantiateViewController(withIdentifier: "UserAgreementVC") as! UserAgreementViewController
+        userAgreementVC.modalPresentationStyle = UIModalPresentationStyle.custom
+        userAgreementVC.transitioningDelegate = self
+        self.present(userAgreementVC, animated: true, completion: nil)
+    }
+    
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return partialVC(presentedViewController: presented, presenting: presenting)
+    }
+    
+    @IBAction func forgetBtnPressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: url)!, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func signUpBtnPressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: url)!, options: [:], completionHandler: nil)
+    }
+    
 
     @IBAction func logIn(_ sender: Any) {
         logInBtn.backgroundColor = UIColor(red:0.75, green:0.75, blue:0.75, alpha:1.0)
@@ -161,4 +214,18 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension LoginViewController: CheckboxDelegate {
+    //кнопка регистрации недоступна, если чекбокс не отмечен
+    func checked(_ checkbox: Checkbox) {
+        if checkbox.isChecked {
+            logInBtn.isEnabled = true
+            logInBtn.backgroundColor = UIColor(red:0.37, green:0.75, blue:0.62, alpha:1.0)
+        }
+        else {
+            logInBtn.isEnabled = false
+            logInBtn.backgroundColor = UIColor(red:0.75, green:0.75, blue:0.75, alpha:1.0)
+        }
+    }
 }
